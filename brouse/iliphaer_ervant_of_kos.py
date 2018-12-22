5b946f82785d923951b4045b844e3b2b12627f4c
Jon Brouse <jbrouse19@gmail.com>
2018-12-22 10:54:12 -0500

    Added iliphaer

---
+++ brouse/iliphaer_ervant_of_kos.py
@@ -0,0 +1,65 @@
+dungeonsheets_version = "0.4.2"
+
+# Basic information
+name = 'Iliphaer Servant of Kos'
+character_class = 'warlock'
+player_name = 'Don'
+background = 'Sailor'
+race = 'Elf'
+level = 1
+alignment = 'Chaotic-Neutral'
+xp = 0
+hp_max = 10
+
+# Ability Scores
+strength = 11
+dexterity = 16
+constitution = 14
+intelligence = 8
+wisdom = 14 
+charisma = 15
+
+# Proficiencies and languages
+skill_proficiencies = [
+    'athletics',
+    'deception',
+    'intimidation',
+    'perception',
+]
+languages = "Common, Elvish."
+
+# Inventory
+cp = 0
+sp = 0
+ep = 0
+gp = 0
+pp = 0
+
+# arcane focus- mentor's wand, spell-book
+weapons = ('light crossbow', 'shortsword', 'dagger')
+armor = 'leather armor'
+shield = ''
+
+equipment = (
+	  """Component pouch, Scholar Pack (backpack, book of lore, ink,
+          ink pen, parchment (10), little bag of sand, small knife""")
+
+# List of known spells
+spells = ('eldritch blast', 'chill touch', 'arms of hadar', 'witch bolt')
+
+# Which spells have been prepared (not including cantrips)
+spells_prepared = ('arms of hadar', 'witch bolt')
+
+# Backstory
+personality_traits = """I never pass up a friendly wager"""
+
+ideals = """The sea is freedom, to go wherever and do anything"""
+
+bonds = """I'll always remember my first sea voyage"""
+
+flaws = """Can't help but pocket loose coins and other trinkets"""
+
+features_and_traits = (
+        """Fey Ancestry (advantage against being charmed, magic cant put me to sleep)
+           Ship's passage (can charter ships free, see book sailors backstory)
+           Awakened Mind (Telepathy 30ft)""")
