013d526aabeb993a5894233662e8a20033a1ab68
Jon Brouse <jbrouse19@gmail.com>
2018-12-21 22:34:57 -0500

    Created new character source

---
+++ brouse/ernest_goodfellow.py
@@ -0,0 +1,70 @@
+dungeonsheets_version = "0.4.2"
+
+# Basic information
+name = 'Ernest Goodfellow'
+character_class = 'wizard'
+player_name = 'Stefan'
+background = "Sage - Apprentice"
+race = 'Human'
+level = 1
+alignment = 'Lawful-Good'
+xp = 0
+hp_max = 8
+
+# Ability Scores
+strength = 11
+dexterity = 12
+constitution = 14
+intelligence = 18
+wisdom = 7 
+charisma = 13
+
+# Proficiencies and languages
+skill_proficiencies = [
+    '',
+]
+languages = "Common, Elvish, Gnomish, Halfling."
+
+# Inventory
+cp = 0
+sp = 0
+ep = 0
+gp = 110
+pp = 0
+
+# arcane focus- mentor's wand, spell-book
+weapons = ('quarterstaff')
+armor = ''
+shield = ''
+equipment = (
+          """Scholar's Pack: Backpack, Book of Lore (Volo's,
+          Guide to Adventuring), Leather Bound Journal,
+          Bottle of Ink, Ink Pen, 10 Sheets of Parchment,
+          A Little Bag of Sand, Small Knife""")
+
+# List of known spells
+spells = ('fire bolt', 'message', 'minor illusion', 'chromatic orb',
+          'disguise self', 'expeditious retreat', 'find familiar',
+          'grease', 'sleep')
+
+# Which spells have been prepared (not including cantrips)
+spells_prepared = ('chromatic orb', 'disguise self', 'expeditious retreat',
+                   'find familiar', 'grease', 'sleep')
+
+# Backstory
+personality_traits = """I have a voracious appetite for books and knowledge.
+                        I sometimes have trouble reading the room. """
+
+ideals = """I never pass up on a chance to learn something new.
+            I seek adventures like those I've read about."""
+
+bonds = """To one day find out what happened to my mother and sister.
+           To advance my training in the arcane arts"""
+
+flaws = """Hopelessly naive and optimistic; assumes all people are good.
+           Defer too much to others."""
+
+features_and_traits = (
+        """Lucky: OOO / day
+
+            Arcane Recovery: 1/day recover one 1st lvl spell slot during short rest""")
